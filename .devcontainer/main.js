357dfeaaea9682d733fba96a2bda1609eaad0bea
aylen25 <aylenarellano5@gmail.com>
2025-05-15 19:44:45 +0000

    html

--- .devcontainer/main.js
+++ .devcontainer/main.js
@@ -1,5 +1,10 @@
 let nombredelavariable ="valor"
 let nombredelavariablenumerica= -6678.987
+
 let variableBoolean=true
+
      variableBoolean=false
+
 let variableNu11 = null
+
+let variablelista=[ "Aylen25" ]
